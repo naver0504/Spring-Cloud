7d292d7da5f2b5e74a48b9342d2f8585d45645ef
naver0504 <qortndwls1234@gmail.com>
2024-03-18 21:01:09 +0900

    feat: health_check, welcome 작성

---
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.userservice.controller;
+
+import com.example.userservice.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final Greeting greeting;
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok(greeting.getMessage());
+    }
+
+
+}
