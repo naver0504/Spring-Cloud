0e0ebb759a472251d698567718c1f0a9c769c154
naver0504 <qortndwls1234@gmail.com>
2024-03-20 18:30:15 +0900

    feat: ModelMapper Bean 등록

---
+++ user-service/src/main/java/com/example/userservice/GlobalConfig.java
@@ -0,0 +1,17 @@
+package com.example.userservice;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GlobalConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+}
