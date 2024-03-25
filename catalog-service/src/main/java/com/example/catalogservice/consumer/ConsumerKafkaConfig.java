dc03d7f38b06b2d93833a420fe5c09eba47e949a
naver0504 <qortndwls1234@gmail.com>
2024-03-26 00:47:11 +0900

    feat: Kafka Consumer TYPE_MAPPING 추가로 인한 에러 해결

---
+++ catalog-service/src/main/java/com/example/catalogservice/consumer/ConsumerKafkaConfig.java
@@ -0,0 +1,46 @@
+package com.example.catalogservice.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class ConsumerKafkaConfig {
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, StockUpdateMessage> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, StockUpdateMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    @Bean
+    public ConsumerFactory<String, StockUpdateMessage> consumerFactory() {
+
+
+
+        Map<String, Object> config = new HashMap<>();
+        /*
+        The TYPE_MAPPINGS property is used to map the type of the message to the class that should be used to deserialize it.
+        매우 중요
+         */
+        config.put(JsonDeserializer.TYPE_MAPPINGS, "OrderMessage:com.example.catalogservice.consumer.StockUpdateMessage");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KafkaConstant.BOOTSTRAP_SERVERS_CONFIG);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, KafkaConstant.GROUP_ID);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, KafkaConstant.AUTO_OFFSET_RESET_CONFIG);
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+}
