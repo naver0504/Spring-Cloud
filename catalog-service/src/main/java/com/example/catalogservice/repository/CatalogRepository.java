a81818087e4c0554d39905b81f7e220c3fc137f4
naver0504 <qortndwls1234@gmail.com>
2024-03-26 00:46:36 +0900

    feat: Optional로 반환하도록 수정

--- catalog-service/src/main/java/com/example/catalogservice/repository/CatalogRepository.java
+++ catalog-service/src/main/java/com/example/catalogservice/repository/CatalogRepository.java
@@ -4,7 +4,9 @@ import com.example.catalogservice.entity.CatalogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CatalogRepository extends JpaRepository<CatalogEntity, Long> {
-    CatalogEntity findByProductId(String productId);
+    Optional<CatalogEntity> findByProductId(String productId);
 }
