55d776476ec163acb4d23f201678914eba6fce51
naver0504 <qortndwls1234@gmail.com>
2024-03-26 00:49:23 +0900

    feat: StockUpdateMessage 작성

--- order-service/src/main/java/com/example/orderservice/service/OrderServiceImpl.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -2,9 +2,15 @@ package com.example.orderservice.service;
 
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.entity.OrderEntity;
+import com.example.orderservice.producer.OrderMessage;
+import com.example.orderservice.producer.KafkaConstant;
 import com.example.orderservice.repository.OrderRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,10 +20,13 @@ import java.util.UUID;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class OrderServiceImpl implements OrderService{
 
     private final ModelMapper modelMapper;
     private final OrderRepository orderRepository;
+    private final KafkaTemplate kafkaTemplate;
+
 
 
     @Override
@@ -28,6 +37,9 @@ public class OrderServiceImpl implements OrderService{
 
         final OrderEntity orderEntity = modelMapper.map(orderDetails, OrderEntity.class);
         orderRepository.save(orderEntity);
+
+        log.info("Catalog Message: {}", OrderMessage.of(orderEntity));
+        kafkaTemplate.send(KafkaConstant.TOPIC, OrderMessage.of(orderEntity));
 
         return modelMapper.map(orderEntity, OrderDto.class);
     }
